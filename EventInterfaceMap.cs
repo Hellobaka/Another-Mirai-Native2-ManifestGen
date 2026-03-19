7b7c0dc1b3438a75338ebbbbd9fe5b5e8d0fc36b
Mr.喜 <863450594@qq.com>
2026-03-19 17:56:41 +0800

    根据抽象实现修复事件Map

--- EventInterfaceMap.cs
+++ EventInterfaceMap.cs
@@ -9,18 +9,21 @@ namespace AMN.ManifestGen
         public static readonly Dictionary<string, (int Type, string Name)> Map = new()
         {
             ["Another_Mirai_Native.Abstractions.Handlers.IGroupMessageHandler"] = ((int)PluginEventType.GroupMsg, "群消息"),
-            ["Another_Mirai_Native.Abstractions.Handlers.IPrivateMessageHandler"] = ((int)PluginEventType.PrivateMsg, "私聊消息"),
-            ["Another_Mirai_Native.Abstractions.Handlers.IDiscussMessageHandler"] = ((int)PluginEventType.DiscussMsg, "讨论组消息"),
+            ["Another_Mirai_Native.Abstractions.Handlers.IPrivateMessageHandle"] = ((int)PluginEventType.PrivateMsg, "私聊消息"),
 
-            ["Another_Mirai_Native.Abstractions.Handlers.IUploadHandler"] = ((int)PluginEventType.Upload, "文件上传"),
+            ["Another_Mirai_Native.Abstractions.Handlers.IGroupFileUploadHandler"] = ((int)PluginEventType.Upload, "文件上传"),
 
             ["Another_Mirai_Native.Abstractions.Handlers.IAdminChangeHandler"] = ((int)PluginEventType.AdminChange, "管理员变更"),
             ["Another_Mirai_Native.Abstractions.Handlers.IGroupMemberDecreaseHandler"] = ((int)PluginEventType.GroupMemberDecrease, "群成员减少"),
             ["Another_Mirai_Native.Abstractions.Handlers.IGroupMemberIncreaseHandler"] = ((int)PluginEventType.GroupMemberIncrease, "群成员增加"),
-            ["Another_Mirai_Native.Abstractions.Handlers.IGroupBanHandler"] = ((int)PluginEventType.GroupBan, "群禁言"),
+            ["Another_Mirai_Native.Abstractions.Handlers.IGroupMemberBannedHandler"] = ((int)PluginEventType.GroupBan, "群成员被禁言"),
+            ["Another_Mirai_Native.Abstractions.Handlers.IGroupMemberUnbannedHandler"] = ((int)PluginEventType.GroupBan, "群成员被解除禁言"),
+            ["Another_Mirai_Native.Abstractions.Handlers.IGroupWholeBannedHandler"] = ((int)PluginEventType.GroupBan, "群全员禁言"),
+            ["Another_Mirai_Native.Abstractions.Handlers.IGroupWholeUnbannedHandler"] = ((int)PluginEventType.GroupBan, "群解除全员禁言"),
+            ["Another_Mirai_Native.Abstractions.Handlers.IGroupInviteRequestHandler"] = ((int)PluginEventType.GroupAddRequest, "受群邀请"),
 
             ["Another_Mirai_Native.Abstractions.Handlers.IFriendAddedHandler"] = ((int)PluginEventType.FriendAdded, "好友添加完成"),
-            ["Another_Mirai_Native.Abstractions.Handlers.IFriendRequestHandler"] = ((int)PluginEventType.FriendRequest, "好友添加请求"),
+            ["Another_Mirai_Native.Abstractions.Handlers.IFriendAddRequestHandler"] = ((int)PluginEventType.FriendRequest, "好友添加请求"),
             ["Another_Mirai_Native.Abstractions.Handlers.IGroupAddRequestHandler"] = ((int)PluginEventType.GroupAddRequest, "加群申请"),
 
             ["Another_Mirai_Native.Abstractions.Handlers.IStartUpHandler"] = ((int)PluginEventType.StartUp, "酷Q启动"),
